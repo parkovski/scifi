65a57b8236c7c4b8de25aa2377f7317162164e10
Parker Snell <parkersnell@gmail.com>
2016-12-17 08:30:22 +1300

    first commit

---
+++ Assets/ParkerMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ParkerMove : MonoBehaviour {
+    Rigidbody2D rb;
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        /*var x = Input.GetAxis("Horizontal") * 0.1f;
+        var y = Input.GetAxis("Vertical") * 0.1f;
+        transform.Translate(x, 0, y);*/
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+        if (horizontal != 0) {
+            rb.AddForce(transform.right * 10f * horizontal);
+        }
+        if (vertical > 0) {
+            if (rb.velocity.y == 0f) {
+                rb.AddForce(transform.up * 8f, ForceMode2D.Impulse);
+            }
+        }
+    }
+}
