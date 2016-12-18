f02df223918ff6454c7191c1305d2b71a9093d45
Parker Snell <parkersnell@gmail.com>
2016-12-17 23:38:59 -0700

    touch & keyboard controls working

--- Assets/ParkerMove.cs
+++ Assets/ParkerMove.cs
@@ -3,47 +3,75 @@ using UnityEngine;
 
 public class ParkerMove : MonoBehaviour {
     Rigidbody2D rb;
-    bool movingLeft;
-    bool movingRight;
+    bool movingLeft = false;
+    bool movingRight = false;
+    bool shouldJump = false;
+    int touchControlLayer;
+    int? leftBtnFingerId = null;
+    int? rightBtnFingerId = null;
 
     // Use this for initialization
     void Start () {
         rb = GetComponent<Rigidbody2D>();
+        touchControlLayer = LayerMask.NameToLayer("Touch Controls");
     }
 
     // Update is called once per frame
     void Update () {
-        /*var horizontal = Input.GetAxis("Horizontal");
-        var vertical = Input.GetAxis("Vertical");
-        if (horizontal != 0) {
-            rb.AddForce(transform.right * 10f * horizontal);
-        }
-        if (vertical > 0) {
-            if (rb.velocity.y == 0f) {
-                rb.AddForce(transform.up * 8f, ForceMode2D.Impulse);
-            }
-        }*/
-        if (movingLeft) {
+        HandleInput();
+
+        if (movingLeft || (leftBtnFingerId != null)) {
             rb.AddForce(transform.right * -10f);
         }
-        if (movingRight) {
+        if (movingRight || (rightBtnFingerId != null)) {
             rb.AddForce(transform.right * 10f);
+        }
+        if (shouldJump) {
+            rb.AddForce(transform.up * 8f, ForceMode2D.Impulse);
+            shouldJump = false;
         }
     }
 
-    public void LeftBtnDown() {
-        movingLeft = true;
-    }
+    void HandleInput() {
+        var horizontalAxis = Input.GetAxis("Horizontal");
+        var verticalAxis = Input.GetAxis("Vertical");
+        if (horizontalAxis < 0) {
+            movingLeft = true;
+            movingRight = false;
+        } else if (horizontalAxis > 0) {
+            movingLeft = false;
+            movingRight = true;
+        } else {
+            movingLeft = movingRight = false;
+        }
 
-    public void LeftBtnUp() {
-        movingLeft = false;
-    }
+        if (verticalAxis != 0) {
+            // TODO: Check for collision with floor instead.
+            if (rb.velocity.y == 0f) {
+                shouldJump = true;
+            }
+        }
 
-    public void RightBtnDown() {
-        movingRight = true;
-    }
-
-    public void RightBtnUp() {
-        movingRight = false;
+        if (Input.touchCount > 0) {
+            foreach (var touch in Input.touches) {
+                if (touch.phase == TouchPhase.Began) {
+                    var ray = Camera.main.ScreenToWorldPoint(touch.position);
+                    var hit = Physics2D.Raycast(ray, Vector2.zero, Mathf.Infinity, 1 << touchControlLayer);
+                    if (hit) {
+                        if (hit.rigidbody.gameObject.name == "left-button") {
+                            leftBtnFingerId = touch.fingerId;
+                        } else if (hit.rigidbody.gameObject.name == "right-button") {
+                            rightBtnFingerId = touch.fingerId;
+                        }
+                    }
+                } else if (touch.phase == TouchPhase.Ended) {
+                    if (touch.fingerId == leftBtnFingerId) {
+                        leftBtnFingerId = null;
+                    } else if (touch.fingerId == rightBtnFingerId) {
+                        rightBtnFingerId = null;
+                    }
+                }
+            }
+        }
     }
 }
