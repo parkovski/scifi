3d466fa24117c0a903afeea14754073e37a0c182
Parker Snell <parkersnell@gmail.com>
2017-01-11 12:31:41 -0800

    electricity for telegraph

--- Assets/Players/Kelvin/Attacks/Telegraph.cs
+++ Assets/Players/Kelvin/Attacks/Telegraph.cs
@@ -4,8 +4,24 @@ namespace SciFi.Items {
     public class Telegraph : MonoBehaviour {
         public GameObject spawnedBy;
 
+        SpriteRenderer electricity1, electricity2;
+        float swapTime;
+        const float frameTime = .1f;
+
         void Start() {
             Item.IgnoreCollisions(gameObject, spawnedBy);
+            electricity1 = gameObject.transform.Find("Electricity1").GetComponent<SpriteRenderer>();
+            electricity2 = gameObject.transform.Find("Electricity2").GetComponent<SpriteRenderer>();
+            electricity2.enabled = false;
+            swapTime = Time.time + frameTime;
+        }
+
+        void Update() {
+            if (Time.time > swapTime) {
+                swapTime = Time.time + frameTime;
+                electricity1.enabled = !electricity1.enabled;
+                electricity2.enabled = !electricity2.enabled;
+            }
         }
 
         void OnCollisionEnter2D(Collision2D collision) {
