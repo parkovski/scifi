b4fa3de265ac6b1fb4e61d46f86582ce520d9043
Parker Snell <parkersnell@gmail.com>
2017-01-31 16:53:13 -0700

    bullet does damage

--- Assets/Players/Nobel/Attacks/Bullet.cs
+++ Assets/Players/Nobel/Attacks/Bullet.cs
@@ -1,9 +1,25 @@
+using UnityEngine;
+
 using SciFi.Items;
 
 namespace SciFi.Players.Attacks {
     public class Bullet : Projectile {
+        public int damage;
+        public float knockback;
+
         public void Start() {
             BaseStart();
+        }
+
+        void OnCollisionEnter2D(Collision2D collision) {
+            var hit = Attack.GetAttackHit(collision.gameObject.layer);
+            if (hit != AttackHit.None) {
+                if (hit == AttackHit.HitAndDamage) {
+                    GameController.Instance.TakeDamage(collision.gameObject, damage);
+                    GameController.Instance.Knockback(gameObject, collision.gameObject, knockback);
+                }
+                Destroy(gameObject);
+            }
         }
     }
 }
