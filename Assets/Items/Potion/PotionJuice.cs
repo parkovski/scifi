5c401f1629c5285395b2b411661fc3da44f1477c
Parker Snell <parkersnell@gmail.com>
2017-02-01 23:59:28 -0700

    potion juice hitting ground animation

--- Assets/Items/Potion/PotionJuice.cs
+++ Assets/Items/Potion/PotionJuice.cs
@@ -2,11 +2,34 @@ using UnityEngine;
 
 namespace SciFi.Items {
     public class PotionJuice : Projectile {
+        public Sprite[] juiceStages;
+        SpriteRenderer spriteRenderer;
+        int stage = 0;
+        float nextStageTime = Mathf.Infinity;
+        const float changeStageTime = 0.02f;
+
         void Start() {
             BaseStart();
+            spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        void Update() {
+            if (Time.time > nextStageTime) {
+                ++stage;
+                if (stage < juiceStages.Length) {
+                    spriteRenderer.sprite = juiceStages[stage];
+                    nextStageTime = Time.time + changeStageTime;
+                } else {
+                    nextStageTime = Mathf.Infinity;
+                }
+            }
         }
 
         void OnCollisionEnter2D(Collision2D collision) {
+            if (stage == 0 && collision.gameObject.tag == "Ground") {
+                nextStageTime = Time.time;
+            }
+
             if (!isServer) {
                 return;
             }
