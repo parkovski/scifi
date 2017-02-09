2a15397581ecee9eec4fc5ddb12993384a21d1aa
Parker Snell <parkersnell@gmail.com>
2017-02-08 22:20:17 -0700

    send positions over state update channel

--- Assets/Game/Network/SFNetworkTransform.cs
+++ Assets/Game/Network/SFNetworkTransform.cs
@@ -1,6 +1,7 @@
 using UnityEngine.Networking;
 
 namespace SciFi.Network {
+    [NetworkSettings(channel = 2)]
     public class SFNetworkTransform : NetworkTransform {
         public override int GetNetworkChannel() {
             return 2;
