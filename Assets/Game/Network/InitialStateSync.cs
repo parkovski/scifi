76a92455aa3d5e28aa5f605238595a10f8f2a1b0
Parker Snell <parkersnell@gmail.com>
2017-02-11 02:13:11 -0700

    doc

--- Assets/Game/Network/InitialStateSync.cs
+++ Assets/Game/Network/InitialStateSync.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 using UnityEngine.Networking;
 
 namespace SciFi.Network {
+    /// The Unity docs claim that you can set forces on an object
+    /// and it will be included in the initial update - unfortunately
+    /// this is not true, and they are only converted into velocities
+    /// by the first update. This waits till then to sync them.
     public class InitialStateSync : NetworkBehaviour {
         [SyncVar]
         Vector2 velocity;
@@ -44,6 +48,10 @@ namespace SciFi.Network {
             var rb = GetComponent<Rigidbody2D>();
             rb.velocity = this.velocity;
             rb.angularVelocity = this.angularVelocity;
+        }
+
+        public override int GetNetworkChannel() {
+            return 0;
         }
     }
 }
