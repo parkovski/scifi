4b666668765ece7c6bf44250945917c8a4a8212c
Parker Snell <parkersnell@gmail.com>
2017-02-25 17:28:17 -0700

    simplify pool reinit

--- Assets/Util/PoolReinit.cs
+++ Assets/Util/PoolReinit.cs
@@ -2,53 +2,12 @@ using UnityEngine;
 
 namespace SciFi.Util {
     public class PoolReinit : MonoBehaviour, IPoolNotificationHandler {
-        public enum ComponentLocation {
-            None,
-            OnObject,
-            InChildren,
-        }
-
-        public ComponentLocation spriteRendererLocation = ComponentLocation.OnObject;
-        public ComponentLocation colliderLocation = ComponentLocation.OnObject;
-
-        void SetState(bool enabled) {
-            if (spriteRendererLocation == ComponentLocation.InChildren) {
-                var srs = GetComponentsInChildren<SpriteRenderer>();
-                foreach (var sr in srs) {
-                    sr.enabled = enabled;
-                }
-            } else if (spriteRendererLocation == ComponentLocation.OnObject) {
-                GetComponent<SpriteRenderer>().enabled = enabled;
-            }
-
-            if (colliderLocation == ComponentLocation.InChildren) {
-                var colls = GetComponentsInChildren<Collider2D>();
-                foreach (var coll in colls) {
-                    coll.enabled = enabled;
-                }
-            } else if (colliderLocation == ComponentLocation.OnObject) {
-                var colls = GetComponents<Collider2D>();
-                foreach (var coll in colls) {
-                    coll.enabled = enabled;
-                }
-            }
-
-            var rb = GetComponent<Rigidbody2D>();
-            if (rb != null) {
-                rb.isKinematic = !enabled;
-                if (!enabled) {
-                    rb.velocity = Vector2.zero;
-                    rb.angularVelocity = 0;
-                }
-            }
-        }
-
         void IPoolNotificationHandler.OnAcquire() {
-            SetState(true);
+            PooledObject.Enable(gameObject);
         }
 
         void IPoolNotificationHandler.OnRelease() {
-            SetState(false);
+            PooledObject.Disable(gameObject);
         }
     }
 }
