6841b03c804030097fcd143576d615b2abd7535e
Parker Snell <parkersnell@gmail.com>
2017-02-28 20:46:33 -0700

    good camera scrolling, don't mess this up

--- Assets/UI/CameraScroll.cs
+++ Assets/UI/CameraScroll.cs
@@ -8,10 +8,19 @@ namespace SciFi.UI {
         public new Camera camera;
         public SpriteRenderer backgroundRenderer;
 
+        Vector3 targetPosition;
+
         float minX;
         float maxX;
         float minY;
         float maxY;
+        /// At 60fps
+        const float maxDelta = 0.1f;
+        const float refocusThreshold = 1f;
+
+        /// If this is true, the camera will aim for a smaller window
+        /// to fit the player into.
+        bool refocus = false;
 
         void Start() {
             float screenHeight = camera.orthographicSize * 2;
@@ -22,35 +31,71 @@ namespace SciFi.UI {
             minY = -maxY;
         }
 
-        const float xwin = 1f;
-        const float ywin = 1f;
+        const float xwin = 2f;
+        const float ywin = 1.5f;
+        const float smallXwin = 0.25f;
+        const float smallYwin = 0.2f;
         void Update() {
             if (playerToFollow == null) {
                 return;
             }
-            var position = playerToFollow.transform.position;
-            var dp = position - camera.transform.position;
+            var dp = playerToFollow.transform.position - camera.transform.position;
+
+            if (refocus) {
+                targetPosition.x = GetTargetPosition(camera.transform.position.x, smallXwin, dp.x);
+                targetPosition.y = GetTargetPosition(camera.transform.position.y, smallYwin, dp.y);
+                if (targetPosition.x == camera.transform.position.x && targetPosition.y == camera.transform.position.y) {
+                    refocus = false;
+                }
+            } else {
+                targetPosition.x = GetTargetPosition(camera.transform.position.x, xwin, dp.x);
+                targetPosition.y = GetTargetPosition(camera.transform.position.y, ywin, dp.y);
+            }
+
+            targetPosition.x = Mathf.Clamp(targetPosition.x, minX, maxX);
+            targetPosition.y = Mathf.Clamp(targetPosition.y, minY, maxY);
+            targetPosition.z = camera.transform.position.z;
+
+            var newCameraPosition = targetPosition;
+            var dx = targetPosition.x - camera.transform.position.x;
+            var dy = targetPosition.y - camera.transform.position.y;
+            var dmax = maxDelta * Time.deltaTime / 0.016667f;
+            if (dx > dmax) {
+                newCameraPosition.x = camera.transform.position.x + dmax;
+                if (dx > refocusThreshold) {
+                    refocus = true;
+                }
+            } else if (dx < -dmax) {
+                newCameraPosition.x = camera.transform.position.x - dmax;
+                if (dx < -refocusThreshold) {
+                    refocus = true;
+                }
+            }
             
-            if (dp.x >= xwin) {
-                position.x = camera.transform.position.x + dp.x - xwin;
-            } else if (dp.x <= -xwin) {
-                position.x = camera.transform.position.x + dp.x + xwin;
-            } else {
-                position.x = camera.transform.position.x;
+            if (dy > dmax) {
+                newCameraPosition.y = camera.transform.position.y + dmax;
+                if (dy > refocusThreshold) {
+                    refocus = true;
+                }
+            } else if (dy < -dmax) {
+                newCameraPosition.y = camera.transform.position.y - dmax;
+                if (dy < -refocusThreshold) {
+                    refocus = true;
+                }
             }
 
-            if (dp.y >= ywin) {
-                position.y = camera.transform.position.y + dp.y - ywin;
-            } else if (dp.y <= -ywin) {
-                position.y = camera.transform.position.y + dp.y + ywin;
-            } else {
-                position.y = camera.transform.position.y;
-            }
+            camera.transform.position = newCameraPosition;
+        }
 
-            position.z = camera.transform.position.z;
-            position.x = Mathf.Clamp(position.x, minX, maxX);
-            position.y = Mathf.Clamp(position.y, minY, maxY);
-            camera.transform.position = position;
+        // offset = player - camera
+        float GetTargetPosition(float position, float maxExtent, float offset, float windowOffset = 0) {
+            if (offset >= maxExtent + windowOffset) {
+                return position + offset - maxExtent - windowOffset;
+            } else if (offset <= -maxExtent + windowOffset) {
+                return position + offset + maxExtent - windowOffset;
+            } else {
+                return position;
+            }
         }
     }
 }
